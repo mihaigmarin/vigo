eb047e6a300a20f50cc8e43cfc35ede85f9bfae9
Gabriel Marin <gabrielmarindev@gmail.com>
2025-11-05 00:35:33 +0100

    starting over to build a cleaner version

--- vigo.go
+++ vigo.go
@@ -2,404 +2,99 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
-	"slices"
-	"strings"
 
 	"github.com/gdamore/tcell/v2"
 )
 
 const (
-	NORMAL_MODE = iota
-	INSERT_MODE
-	COMMAND_MODE
+	NORMAL = iota
+	INSERT
+	COMMAND
 )
 
-// Structure for holding cursor position
-type cursor struct {
-	x            int
-	y            int
-	scrolloffset int
-}
-
-// Structure for holding editor state.
 type editor struct {
-	screen    tcell.Screen
-	style     tcell.Style
-	mode      int
-	lines     []string
-	cursor    cursor
-	cmdbuf    string
-	statusmsg string
-	fname     string
-	lastkey   rune
+	fname  string
+	c      cursor
+	lines  []string
+	screen tcell.Screen
+	style  tcell.Style
+	mode   int
 }
 
-// Function for initializing cursor state based on file size
-func initCursor(c *cursor, lines []string) {
-	c.x = 0
-	c.y = 0
-	c.scrolloffset = 0
-	c.scrolloffset = 0
-	if c.y+c.scrolloffset >= len(lines) {
-		c.y = len(lines) - 1 - c.scrolloffset
-		if c.y < 0 {
-			c.y = 0
-		}
-	}
+type cursor struct {
+	x      int
+	y      int
+	offset int
 }
 
-// Function for initializing editor state. As parameters it needs
-// an editor structure variable and an array of lines
-func initEditor(e *editor, lines []string) {
+// Init editor
+func (e *editor) init() {
 	var err error
+	e.fname = ""
+	e.c.x = 0
+	e.c.y = 0
+	e.c.offset = 0
+	e.lines = make([]string, 0)
 	e.screen, err = tcell.NewScreen()
 	if err != nil {
-		log.Fatalf("Failed to create screen: %v", err)
+		log.Fatal(err)
 	}
-	if err := e.screen.Init(); err != nil {
-		log.Fatalf("Failed to initialize screen: %v", err)
-	}
-	e.style = tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorBlack)
-	e.mode = NORMAL_MODE
-	e.lines = lines
-	e.cmdbuf = ""
-	e.statusmsg = ""
-	e.fname = os.Args[1]
-	e.lastkey = 0
-
-	initCursor(&e.cursor, e.lines)
-
-	// Draw initial lines
-	draw(e)
-}
-
-// Draw lines that are stored inside editor structure to screen
-func draw(e *editor) {
-	e.screen.Clear()
-	width, height := e.screen.Size()
-
-	// Draw lines
-	for y := 0; y < height && (e.cursor.scrolloffset+y) < len(e.lines); y++ {
-		line := e.lines[e.cursor.scrolloffset+y]
-		for x, r := range line {
-			if x >= width {
-				break
-			}
-			e.screen.SetContent(x, y, r, nil, e.style)
-		}
-	}
-
-	// Draw status line
-	// TODO: create a structure to hold status bar state
-	var status string
-	if e.mode == COMMAND_MODE {
-		status = ":" + e.cmdbuf
-	} else if e.statusmsg != "" {
-		status = e.statusmsg
-	} else if e.mode == INSERT_MODE {
-		status = "-- INSERT --"
-	}
-
-	for i := 0; i < width; i++ {
-		ch := ' '
-		if i < len(status) {
-			ch = rune(status[i])
-		}
-		e.screen.SetContent(i, height-1, ch, nil, e.style)
-	}
-
-	// Show cursor position
-	currline := ""
-	if e.cursor.y+e.cursor.scrolloffset < len(e.lines) {
-		currline = e.lines[e.cursor.y+e.cursor.scrolloffset]
-	}
-	if e.cursor.x > len(currline) {
-		e.cursor.x = len(currline)
-	}
-	e.screen.ShowCursor(e.cursor.x, e.cursor.y)
-
-	e.screen.Show()
-}
-
-// Write lines to a file
-func wlines(filename string, lines []string) error {
-	file, err := os.Create(filename)
+	err = e.screen.Init()
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	for _, line := range lines {
-		_, err := writer.WriteString(line + "\n")
-		if err != nil {
-			return err
-		}
-	}
-	return writer.Flush()
+	e.style = tcell.StyleDefault.Normal()
+	e.mode = NORMAL
 }
 
-// Handle NORMAL_MODE functionalities
-func handleNormalMode(e *editor, ev *tcell.EventKey) {
-	lineIdx := e.cursor.y + e.cursor.scrolloffset
-	_, height := e.screen.Size()
-	switch ev.Key() {
-	case tcell.KeyCtrlQ, tcell.KeyEscape:
-		return
-	default:
-		switch ev.Rune() {
-		case 'q':
-			return
-		case 'x':
-			if lineIdx < len(e.lines) && e.cursor.x > 0 {
-				line := e.lines[lineIdx]
-				e.lines[lineIdx] = line[:e.cursor.x] + line[e.cursor.x+1:]
-			}
-		case 'g':
-			if e.lastkey == 'g' {
-				e.cursor.scrolloffset = len(e.lines) - height + 1
-				e.cursor.y = len(e.lines) - e.cursor.scrolloffset - 1
-				e.cursor.x = 0
-				e.lastkey = 0
-			} else {
-				e.lastkey = 'g'
-			}
-		case 'G':
-			if e.lastkey == 'G' {
-				e.cursor.scrolloffset = 0
-				e.cursor.y = 0
-				e.cursor.x = 0
-				e.lastkey = 0
-			} else {
-				e.lastkey = 'G'
-			}
-		case '$': // go to the end of the current line
-			e.cursor.x = len(e.lines[lineIdx])
-		case '0': // go to the start of the current line
-			e.cursor.x = 0
-		case 'd':
-			if e.lastkey == 'd' {
-				if lineIdx < len(e.lines) {
-					e.lines = slices.Delete(e.lines, lineIdx, lineIdx+1)
-					if len(e.lines) == 0 {
-						e.lines = append(e.lines, "")
-					}
-					if e.cursor.y+e.cursor.scrolloffset >= len(e.lines) {
-						if e.cursor.y > 0 {
-							e.cursor.y--
-						} else if e.cursor.scrolloffset > 0 {
-							e.cursor.scrolloffset--
-						}
-					}
-				}
-				e.lastkey = 0 // reset
-			} else {
-				e.lastkey = 'd'
-			}
-		case 'i':
-			e.mode = INSERT_MODE
-		case 'o': // go in insert mode and add a new line down
-			e.mode = INSERT_MODE
-			lineIdx := e.cursor.y + e.cursor.scrolloffset
-			if lineIdx < len(e.lines) {
-				// Insert new line with 'after' part
-				e.lines = append(e.lines[:lineIdx+1], append([]string{""}, e.lines[lineIdx+1:]...)...)
-				e.cursor.y++
-				e.cursor.x = 0
-
-				// If cursor is at bottom of screen, scroll
-				_, h := e.screen.Size()
-				if e.cursor.y >= h-1 {
-					e.cursor.scrolloffset++
-					e.cursor.y = h - 2
-				}
-			}
-		case 'O': // go in insert mode and add a new line up
-			e.mode = INSERT_MODE
-			lineIdx := e.cursor.y + e.cursor.scrolloffset
-			if lineIdx < len(e.lines) {
-				// Insert new line with 'before' current line
-				e.lines = append(e.lines[:lineIdx], append([]string{""}, e.lines[lineIdx:]...)...)
-				e.cursor.x = 0
-			}
-		case 'j': // scroll down
-			// NOTE: if we are at the last line displayed on screen then
-			// change scrolloffset, else just change the cursor position
-			if e.cursor.scrolloffset+e.cursor.y+1 < len(e.lines) {
-				_, h := e.screen.Size() // Screen height
-				if e.cursor.y < h-2 {
-					e.cursor.y++
-				} else {
-					e.cursor.scrolloffset++
-				}
-			}
-		case 'k': // scroll up
-			if e.cursor.y > 0 {
-				e.cursor.y--
-			} else if e.cursor.scrolloffset > 0 {
-				e.cursor.scrolloffset--
-			}
-		case 'h': // move left
-			if e.cursor.x > 0 {
-				e.cursor.x--
-			}
-		case 'l': // move right
-			lineIdx := e.cursor.y + e.cursor.scrolloffset
-			if lineIdx < len(e.lines)-1 && e.cursor.x < len(e.lines[lineIdx])-1 {
-				e.cursor.x++
-			}
-		case ':':
-			e.mode = COMMAND_MODE
-			e.cmdbuf = ""
-			e.statusmsg = ""
+// Draw editor content on screen
+func (e *editor) draw() {
+	e.screen.Clear()
+	//h, w := e.screen.Size()
+	for y, l := range e.lines {
+		for x, c := range l {
+			e.screen.SetContent(x, y, c, nil, e.style)
 		}
 	}
-}
-
-// Handle INSERT_MODE functionalities
-func handleInsertMode(e *editor, ev *tcell.EventKey) {
-	lineIdx := e.cursor.y + e.cursor.scrolloffset
-	switch ev.Key() {
-	case tcell.KeyESC:
-		e.mode = NORMAL_MODE
-		// Here we handle deleting in insert mode
-	case tcell.KeyBackspace, tcell.KeyBackspace2:
-		if lineIdx < len(e.lines) && e.cursor.x > 0 {
-			line := e.lines[lineIdx]
-			e.lines[lineIdx] = line[:e.cursor.x-1] + line[e.cursor.x:]
-			e.cursor.x--
-		}
-	case tcell.KeyEnter:
-		if lineIdx < len(e.lines) {
-			line := e.lines[lineIdx]
-			// Split the line at cursorX
-			before := line[:e.cursor.x]
-			after := line[e.cursor.x:]
-			// Insert new line with 'after' part
-			e.lines = append(e.lines[:lineIdx+1], append([]string{after}, e.lines[lineIdx+1:]...)...)
-			e.lines[lineIdx] = before
-			e.cursor.y++
-			e.cursor.x = 0
-
-			// If cursor is at bottom of screen, scroll
-			_, h := e.screen.Size()
-			if e.cursor.y >= h-1 {
-				e.cursor.scrolloffset++
-				e.cursor.y = h - 2
-			}
-		}
-		// Here we handle writing characters in insert mode
-	case tcell.KeyRune:
-		lineIdx := e.cursor.y + e.cursor.scrolloffset
-		if lineIdx < len(e.lines) {
-			line := e.lines[lineIdx]
-			r := ev.Rune()
-			if e.cursor.x > len(line) {
-				e.cursor.x = len(line)
-			}
-			e.lines[lineIdx] = line[:e.cursor.x] + string(r) + line[e.cursor.x:]
-			e.cursor.x++
-		}
-	}
-}
-
-// Handle COMMAND_MODE functionalities
-func handleCommandMode(e *editor, ev *tcell.EventKey) {
-	switch ev.Key() {
-	case tcell.KeyEsc:
-		e.mode = NORMAL_MODE
-		e.cmdbuf = ""
-		e.statusmsg = ""
-	case tcell.KeyEnter:
-		cmd := strings.TrimSpace(e.cmdbuf)
-
-		// NOTE: Here we handle commands in COMMAND_MODE
-		switch cmd {
-		case "w":
-			err := wlines(e.fname, e.lines)
-			if err != nil {
-				e.statusmsg = fmt.Sprintf("Error writing: %v", err)
-			} else {
-				e.statusmsg = fmt.Sprintf("Wrote to %s", e.fname)
-			}
-		case "q":
-			e.screen.Fini()
-			os.Exit(0)
-		case "wq":
-			err := wlines(e.fname, e.lines)
-			if err != nil {
-				e.statusmsg = fmt.Sprintf("Error writing: %v", err)
-				e.mode = NORMAL_MODE
-			} else {
-				e.screen.Fini()
-				os.Exit(0)
-			}
-		default:
-			e.statusmsg = "Unknown command: " + cmd
-			e.mode = NORMAL_MODE
-		}
-	case tcell.KeyBackspace, tcell.KeyBackspace2:
-		if len(e.cmdbuf) > 0 {
-			e.cmdbuf = e.cmdbuf[:len(e.cmdbuf)-1]
-		}
-	case tcell.KeyRune:
-		e.cmdbuf += string(ev.Rune())
-	}
+	e.screen.ShowCursor(e.c.x, e.c.y)
+	e.screen.Show()
 }
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatalf("Usage: %s <filename>", os.Args[0])
+		log.Fatal("Please provide file name")
 	}
-	filename := os.Args[1]
-
-	// Try to open file, create if it doesn't exist
-	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 644)
+	fname := os.Args[1]
+	f, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		log.Fatalf("Failed to open file: %v", err)
+		log.Fatal(err)
 	}
-	defer file.Close()
+	defer f.Close()
 
-	// Read file line by line
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("Failed to read file: %v", err)
-	}
-
-	// Handle empty file
-	if len(lines) == 0 {
-		lines = append(lines, "")
-	}
-
-	// Init editor structure
 	var e editor
-	initEditor(&e, lines)
-	defer e.screen.Fini() // NOTE: doesn't work when added in 'initEditor()'
-
-	// This is the main application loop. Here we handle all keys
+	e.init()
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		l := s.Text()
+		e.lines = append(e.lines, l)
+	}
 	for {
 		ev := e.screen.PollEvent()
-
 		switch ev := ev.(type) {
-		case *tcell.EventKey:
-			switch e.mode {
-			case NORMAL_MODE:
-				handleNormalMode(&e, ev)
-			case INSERT_MODE:
-				handleInsertMode(&e, ev)
-			case COMMAND_MODE:
-				handleCommandMode(&e, ev)
-			}
-			draw(&e)
 		case *tcell.EventResize:
-			draw(&e)
+			e.draw()
+		case *tcell.EventKey:
+			switch ev.Key() {
+			case tcell.KeyCtrlQ:
+				e.screen.Fini()
+				os.Exit(0)
+			default:
+			}
+		default:
+			e.draw()
 		}
+		e.draw()
 	}
 }
